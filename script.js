adf345a6f9a9d551bcaa00b6b19e735f6042abf7
Joe Glendenning <joe.kodehode@gmail.com>
2025-08-05 12:09:25 +0200

    fruit catalog object array

---
+++ script.js
@@ -0,0 +1,45 @@
+const fruits = [
+  { name: "Apple", color: "Red", calories: 52, pricePerKg: 3.5, origin: "USA" },
+  {
+    name: "Banana",
+    color: "Yellow",
+    calories: 89,
+    pricePerKg: 1.2,
+    origin: "Ecuador",
+  },
+  {
+    name: "Orange",
+    color: "Orange",
+    calories: 47,
+    pricePerKg: 2.8,
+    origin: "Spain",
+  },
+  {
+    name: "Strawberry",
+    color: "Red",
+    calories: 32,
+    pricePerKg: 6.0,
+    origin: "Mexico",
+  },
+  {
+    name: "Mango",
+    color: "Orange",
+    calories: 60,
+    pricePerKg: 4.0,
+    origin: "India",
+  },
+  {
+    name: "Grapes",
+    color: "Green",
+    calories: 69,
+    pricePerKg: 2.5,
+    origin: "Italy",
+  },
+  {
+    name: "Pineapple",
+    color: "Brown",
+    calories: 50,
+    pricePerKg: 3.0,
+    origin: "Costa Rica",
+  },
+];
